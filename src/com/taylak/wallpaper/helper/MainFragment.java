d34d495369b096452fc63467fd9348f27cf01992
agacagatay <ahmetce06@gmail.com>
2014-09-30 17:52:29 +0300

    j

---
+++ src/com/taylak/wallpaper/helper/MainFragment.java
@@ -0,0 +1,23 @@
+package com.taylak.wallpaper.helper;
+
+ 
+import com.taylak.wallpaperhd.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		 View view = inflater.inflate(R.layout.activity_wcw, container, false);
+
+		    return view;
+	}
+
+}
